27abb465d3164c9e94556e0ead67d1a8709d8819
Kiminjo <deulseong1121@gmail.com>
2021-05-23 16:18:21 +0900

    edit code for output

--- generate.py
+++ generate.py
@@ -30,25 +30,36 @@ def generate(model, seed_characters, temperature, network_type, char2int_dict, i
     hidden = model.init_hidden(1)
     
     for string in seed_characters :
-        x = np.array([[char2int_dict(string)]])
-        x = one_hot_encoding(x)
-        x = torch.from_numpy(x).to(device)
-        
-        if network_type == 'RNN' :
-            hidden = tuple([each.data for each in hidden])[0].reshape(1, -1, 512)
-        else :
-            hidden = tuple([each.data for each in hidden])
-            
-        output, hidden = model.forward(x, hidden)
-        
-        prob = F.softmax(output, dim=1).data
-        prob, top_char = prob.topk(temperature)
-        top_char = top_char.numpy().squeeze()
-        prob = prob.numpy().squeeze()
-        
-        character = np.random.choice(top_char, p=prob/prob.sum())
-        character = int2char_dict[character]
-        
+        character, hidden = predict(model, string, hidden, temperature, char2int_dict, int2char_dict, device, network_type)
+
     samples.append(character)
+    
+    for i in range(2000) :
         
-    return samples
+        char, hidden = predict(model, samples[-1], hidden, temperature, char2int_dict, int2char_dict, device, network_type)
+        samples.append(char)
+        
+    return ''.join(samples)
+
+
+def predict(model, string, hidden, temperature, char2int_dict, int2char_dict, device, network_type) :
+    x = torch.tensor([[char2int_dict[string]]], dtype=torch.float)
+    x = one_hot_encoding(x)
+    x = x.to(device)
+    
+    if network_type == 'RNN' :
+        hidden = tuple([each.data for each in hidden])[0].reshape(1, -1, 512)
+    else :
+        hidden = tuple([each.data for each in hidden])
+        
+    output, hidden = model.forward(x, hidden)
+    
+    prob = F.softmax(output, dim=1).data
+    prob, top_char = prob.topk(temperature)
+    top_char = top_char.numpy().squeeze()
+    prob = prob.numpy().squeeze()
+    
+    character = np.random.choice(top_char, p=prob/prob.sum())
+    character = int2char_dict[character]
+    
+    return character, hidden
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Created on Mon May 17 22:40:56 2021
 
 import torch
 from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
 from dataset import Shakespeare, one_hot_encoding
 from model import CharRNN, CharLSTM
 from generate import generate
@@ -41,7 +42,7 @@ def train(model, trn_loader, device, criterion, optimizer, batch_size, network_t
         x = one_hot_encoding(x)
         x = x.to(device); label = label.to(device)
         if network_type=='RNN' :
-            hidden = tuple([each.data for each in hidden])[0].reshape(1, -1, 512)
+            hidden = tuple([each.data for each in hidden])[0].reshape(model.num_layer, 100, model.hidden_size)
         else :
             hidden = tuple([each.data for each in hidden])
         optimizer.zero_grad()
@@ -111,8 +112,9 @@ def main():
     """
     
     input_file = open('data/shakespeare_train.txt', 'r').read()
-    epochs = 3
+    epochs = 5
     batch_size = 100
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
     
     train_dataset = Shakespeare(input_file, is_train=True)
     test_dataset = Shakespeare(input_file, is_train=False)
@@ -121,34 +123,16 @@ def main():
     test_data = DataLoader(test_dataset, batch_size=batch_size)
     
     ##################################################################
-    #                   LSTM model
-    ##################################################################
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    lstm_model = CharLSTM(input_size=len(train_dataset.char2int), 
-                    hidden_size=512, num_layer=1).to(device)
-    lstm_optimizer = torch.optim.Adam(lstm_model.parameters(), lr=0.01)
-    lstm_criterion = torch.nn.CrossEntropyLoss().to(device)
-    
-    lstm_trn_loss = []; lstm_val_loss = []
-    for epoch in range(epochs) :
-        train_loss = train(lstm_model, train_data, device, lstm_criterion, lstm_optimizer, batch_size, 'LSTM')
-        test_loss = validate(lstm_model, test_data, device, lstm_criterion, batch_size, 'LSTM')
-        print('epoch : {}, train loss : {}, validation loss : {}'.format(epoch+1, train_loss, test_loss))
-        
-        lstm_trn_loss.append(train_loss); lstm_val_loss.append(test_loss)
-        
-    
-    
-    ##################################################################
     #                   RNN model
     ##################################################################
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
     rnn_model = CharRNN(input_size=len(train_dataset.char2int), 
                     hidden_size=512, num_layer=1).to(device)
     rnn_optimizer = torch.optim.Adam(rnn_model.parameters(), lr=0.01)
     rnn_criterion = torch.nn.CrossEntropyLoss().to(device)
     
     rnn_trn_loss = []; rnn_val_loss = []
+    
+    print('RNN training start')
     for epoch in range(epochs) :
         train_loss = train(rnn_model, train_data, device, rnn_criterion, rnn_optimizer, batch_size, 'RNN')
         test_loss = validate(rnn_model, test_data, device, rnn_criterion, batch_size, 'RNN')
@@ -156,14 +140,62 @@ def main():
         
         rnn_trn_loss.append(train_loss); rnn_val_loss.append(test_loss)
         
+    rnn_generated_text = generate(rnn_model, 'The', 5, 'RNN', train_dataset.char2int, train_dataset.int2char)
+    rnn_text = open('rnn.txt', 'w')
+    rnn_text.write(rnn_generated_text)
+    rnn_text.close()
     
-    lstm_generated_text = generate(lstm_model, 'The', 5, 'LSTM', train_dataset.char2int, train_dataset.int2char)
-    #rnn_generated_text = 
+    
+    
+    ##################################################################
+    #                   LSTM model
+    ##################################################################
+    lstm_model = CharLSTM(input_size=len(train_dataset.char2int), 
+                    hidden_size=512, num_layer=1).to(device)
+    lstm_optimizer = torch.optim.Adam(lstm_model.parameters(), lr=0.01)
+    lstm_criterion = torch.nn.CrossEntropyLoss().to(device)
+    
+    lstm_trn_loss = []; lstm_val_loss = []
+    print('\n LSTM training start')
+    
+    for epoch in range(epochs) :
+        train_loss = train(lstm_model, train_data, device, lstm_criterion, lstm_optimizer, batch_size, 'LSTM')
+        test_loss = validate(lstm_model, test_data, device, lstm_criterion, batch_size, 'LSTM')
+        print('epoch : {}, train loss : {}, validation loss : {}'.format(epoch+1, train_loss, test_loss))
         
+        lstm_trn_loss.append(train_loss); lstm_val_loss.append(test_loss)
+        
+        
+    lstm_generated_text = generate(lstm_model, 'The', 5, 'LSTM', train_dataset.char2int, train_dataset.int2char)
+    lstm_text = open('lstm.txt', 'w')
+    lstm_text.write(lstm_generated_text)
+    lstm_text.close()
+    
+    draw_result_plot(rnn_trn_loss, rnn_val_loss, lstm_trn_loss, lstm_val_loss)
+    
 
-    return lstm_trn_loss, lstm_val_loss, lstm_generated_text
 
-
-
+def draw_result_plot(rnn_trn, rnn_val, lstm_trn, lstm_val) :
+    fig, axes = plt.subplots(ncols=2, nrows=2, figsize=(20, 20))
+    rnn_epoch = list(range(len(rnn_trn)))
+    lstm_epoch = list(range(len(lstm_trn)))
+    
+    axes[0, 0].plot(rnn_epoch, rnn_trn)
+    axes[0, 0].set_title('RNN model train loss')
+    
+    axes[0, 1].plot(rnn_epoch, rnn_val)
+    axes[0, 1].set_title('RNN model validation loss')
+    
+    axes[1, 0].plot(lstm_epoch, lstm_trn)
+    axes[1, 0].set_title('LSTM model train loss')
+    
+    axes[1, 1].plot(lstm_epoch, lstm_val)
+    axes[1, 1].set_title('LSTM model validation loss')
+    
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.savefig('result.png')
+    
+    
 if __name__ == '__main__':
-    train_loss, val_loss, generated_text = main()
+     main()
